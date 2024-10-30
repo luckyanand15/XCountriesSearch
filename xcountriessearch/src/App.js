4eb4b052f5bc519e03f4439754d0e5fe77bbb9d0
luckyanand15 <luckyanand.741@gmail.com>
2024-10-30 18:04:14 +0530

    update 5

--- xcountriessearch/src/App.js
+++ xcountriessearch/src/App.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Styles from "./App.module.css";
 import axios from "axios";
-import Countries from "./components/Countries";
 
 function App() {
   const [data, setData] = useState([]);
-  const [searchData,setSearchData] = useState("");
-  
+  const [searchData, setSearchData] = useState("");
+
   useEffect(() => {
     const getCountries = async () => {
       try {
@@ -19,35 +18,42 @@ function App() {
     getCountries();
   }, []);
 
-  const filterData = data.filter((country)=>country.name.common.toLowerCase().includes(searchData.toLowerCase()));
+  const filterData = data.filter((country) =>
+    country.name.common.toLowerCase().includes(searchData.toLowerCase())
+  );
   return (
     <div>
       <div className={Styles.search}>
-        <input type="text" placeholder="Search for countries..." value={searchData} onChange={(e)=>setSearchData(e.target.value)}/>
+        <input
+          type="text"
+          placeholder="Search for countries..."
+          value={searchData}
+          onChange={(e) => setSearchData(e.target.value)}
+        />
       </div>
-      {filterData.length>0 || searchData.length>0 ? 
-      (
+      {filterData.length > 0 || searchData.length > 0 ? (
         <div className={Styles.wrapper}>
-        {filterData.map((country) => {
-          return (
-            <div key={`${country.cca3}`}>
-              <Countries name={country.name.common} flag={country.flags.png}/>
-            </div>
-          );
-        })}
-      </div>
-      ):(
+          {filterData.map((country) => {
+            return (
+              <div className={Styles.countryCard}>
+                <img src={country.flags.png} alt={country.name.common} />
+                <p>{country.name.common}</p>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
         <div className={Styles.wrapper}>
-        {data.map((country) => {
-          return (
-            <div key={`${country.cca3}`}>
-              <Countries name={country.name.common} flag={country.flags.png}/>
-            </div>
-          );
-        })}
-      </div>
+          {data.map((country) => {
+            return (
+              <div className={Styles.countryCard}>
+                <img src={country.flags.png} alt={country.name.common} />
+                <p>{country.name.common}</p>
+              </div>
+            );
+          })}
+        </div>
       )}
-      
     </div>
   );
 }
